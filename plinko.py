15281072e9ca7451c22deadcf0843a622819b544
jovan <git.keseljevicjovan@proton.me>
2024-12-10 01:57:13 +0100

    Make gradient for bins

--- plinko.py
+++ plinko.py
@@ -30,7 +30,7 @@ BIN_HEIGHT = int(60 * RATIO)
 BIN_WIDTH = WIDTH // BIN_COUNT
 BIN_LABELS = [round(i * 0.2, 1) for i in range(BIN_COUNT)]
 BIN_TEXTS = ['1000', '130x', '26x', '9x', '4x', '2x', '0.2x', '0.2x', '0.2x','0.2x','0.2x','2x','4x','9x','26x', '130x', '1000']
-hit_bins = []  # List to store hit bins
+hit_bins = []
 
 # Fonts for the headers
 font = 'Gill Sans'
@@ -39,8 +39,7 @@ header1 = pygame.font.SysFont(font, int(22 * RATIO), True)
 header2 = pygame.font.SysFont(font, int(14 * RATIO), True)
 header_money = pygame.font.SysFont(font, int(36 * RATIO), True)
 
-# Define other variables for bin rendering
-bin_width = BIN_WIDTH // 2  # Adjusted width of the bins
+bin_width = BIN_WIDTH // 2
 
 def create_pins():
     pins.clear()
@@ -58,16 +57,41 @@ def draw_pins():
 def create_bin_text_surfaces():
     bin_text_surfaces = []
     texts = BIN_TEXTS
-    if PIN_ROWS < 10:  # Assuming this is for some special case
-        texts = BIN_TEXTS  # You might want to define low_bin_texts elsewhere
+    if PIN_ROWS < 10:
+        texts = BIN_TEXTS
     for text in texts:
         rendered_text = header2.render(text, True, BLACK)
         bin_text_surfaces.append(rendered_text)
     return bin_text_surfaces
 
 def draw_rounded_rect(surface, rect, color, radius, border_radius=0):
-    """Draws a rounded rectangle."""
     pygame.draw.rect(surface, color, rect, border_radius)
+
+def create_rgb_gradient(start_color, end_color, steps):
+    step_red = (end_color[0] - start_color[0]) / (steps - 1)
+    step_green = (end_color[1] - start_color[1]) / (steps - 1)
+    step_blue = (end_color[2] - start_color[2]) / (steps - 1)
+
+    gradient = [
+        (
+            int(start_color[0] + step_red * i),
+            int(start_color[1] + step_green * i),
+            int(start_color[2] + step_blue * i)
+        )
+        for i in range(steps)
+    ]
+    return gradient
+
+# Define gradient colors
+START_COLOR = (253, 0, 65)  # Red
+CENTER_COLOR = (254, 190, 11)  # Yellow
+END_COLOR = START_COLOR
+
+# Gradient
+gradient_steps = BIN_COUNT // 2
+left_gradient = create_rgb_gradient(START_COLOR, CENTER_COLOR, gradient_steps + 1)
+right_gradient = create_rgb_gradient(CENTER_COLOR, END_COLOR, gradient_steps + 1)[1:]
+bin_colors = left_gradient + right_gradient
 
 def render_bins():
     global hit_bins
@@ -76,7 +100,7 @@ def render_bins():
     bin_start_offset = WIDTH // 2 - (PIN_ROWS // 2 + 0.5) * PIN_SPACING
     base_y = PIN_ROWS * PIN_SPACING + PIN_START + PIN_SPACING // 2
     for bin in range(PIN_ROWS + 1):
-        animate = True if bin in hit_bins else False  # if hit
+        animate = True if bin in hit_bins else False
 
         offset_x = bin * PIN_SPACING + (not PIN_ROWS % 2) * PIN_SPACING // 2
         index = bin + 8 - PIN_ROWS // 2
@@ -84,27 +108,20 @@ def render_bins():
             index -= 1
 
         bin_text_surface = bin_text_surfaces[index]
-
         base_x = bin_start_offset - bin_width // 2 + offset_x
+        bin_color = bin_colors[bin]
 
         if animate:
-            # Configure Rects for drawing  
             light_rect_right = pygame.Rect(base_x, base_y + click_offset, bin_width, bin_width)
             text_rect = bin_text_surface.get_rect(center=(base_x + bin_width // 2, base_y + bin_width // 2 + click_offset))
-
-            # Remove animated bin
             hit_bins = list(filter(lambda x: x != bin, hit_bins))
         else:
-            # Configure Rects for drawing  
             dark_rect_right = pygame.Rect(base_x, base_y + click_offset, bin_width, bin_width)       
             light_rect_right = pygame.Rect(base_x, base_y, bin_width, bin_width)
             text_rect = bin_text_surface.get_rect(center=(base_x + bin_width // 2, base_y + bin_width // 2))
+            draw_rounded_rect(screen, dark_rect_right, bin_color, int(4 * RATIO) + (RATIO > 1))
 
-            # Draw dark rectangle
-            draw_rounded_rect(screen, dark_rect_right, GREEN, int(4 * RATIO) + (RATIO > 1))
-
-        # Draw light rectangle and text
-        draw_rounded_rect(screen, light_rect_right, GREEN, int(4 * RATIO) + (RATIO > 1))
+        draw_rounded_rect(screen, light_rect_right, bin_color, int(4 * RATIO) + (RATIO > 1))
         screen.blit(bin_text_surface, text_rect)
 
 def draw_bins(): 
@@ -128,4 +145,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
